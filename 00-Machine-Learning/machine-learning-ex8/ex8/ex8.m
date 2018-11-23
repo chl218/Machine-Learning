de69995eb6cb8345df28f5e08c73029221f01a2d
Jack <chl218@eng.ucsd.edu>
2018-11-23 11:14:24 -0800

    threshold selection with F1 score

--- 00-Machine-Learning/machine-learning-ex8/ex8/ex8.m
+++ 00-Machine-Learning/machine-learning-ex8/ex8/ex8.m
@@ -75,7 +75,7 @@ fprintf('Program %paused. Press enter to continue.\n');
 
 pval = multivariateGaussian(Xval, mu, sigma2);
 
-[epsilon F1] = selectThreshold(yval, pval);
+[epsilon, F1] = selectThreshold(yval, pval);
 fprintf('Best epsilon found using cross-validation: %e\n', epsilon);
 fprintf('Best F1 on Cross Validation Set:  %f\n', F1);
 fprintf('   (you should see a value epsilon of about 8.99e-05)\n');
